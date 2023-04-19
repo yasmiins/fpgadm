11cf5fa13a2843c56e1cefc0cbffd5addd04812a
Yasmin <49572952+yasmin-shahed@users.noreply.github.com>
2023-04-19 04:02:11 -0400

    modes added

--- top.sv
+++ top.sv
@@ -15,7 +15,72 @@ module top (
   input  logic txready, rxready
 );
 
+  logic [4:0] keycode;
+  logic strobe;
+  logic [1:0] mode;
   
+  scankey sk (.clk(hz2m), .rst(reset), .in(pb[19:0]), .out(keycode), .strobe(strobe));
+  controller clr(.clk(strobe), .rst(reset), .set_edit(keycode == 5'b10011), .set_play(keycode == 5'b10010), .set_raw(keycode == 5'b10000), .mode(mode));
+
+  assign red = mode == 2'd2;
+  assign green = mode == 2'd1;
+  assign blue = mode == 2'd0;
+  
+  logic [7:0] edit_seq_out;
+  logic [2:0] ctr_out;
+  logic [3:0] edit_play_smpl [7:0];
+  logic hz2;
+  logic [7:0] play_seq_out;
+  logic [2:0] seq_sel;
+  logic [7:0] seq_out;
+  
+  clkdiv divhz2 (.clk(hz2m), .rst(reset), .lim(8'b00011000), .hzX(hz2));
+  sequencer edit (.clk(strobe), .rst(reset), .srst(mode != 2'd0), .go_right(pb[8]), .go_left(pb[11]), .seq_out(edit_seq_out));
+  prienc8to3 enc (.in(edit_seq_out), .out(ctr_out));
+
+  sequencer play (.clk(hz2), .rst(reset), .srst(mode != 2'd1), .go_right(1'b1), .go_left(1'b0), .seq_out(play_seq_out));
+  
+  always_ff @(posedge hz2m, posedge reset) begin
+    if (reset) begin
+      seq_out <= 8'b0;
+    end else begin
+      seq_out <= mode == 2'd1 ? play_seq_out : edit_seq_out;
+    end
+  end
+  
+  prienc8to3 enc2 (.in(seq_out), .out(seq_sel));
+
+  assign {ss7[5], ss7[1], ss7[4], ss7[2]} = {edit_play_smpl[7][3], edit_play_smpl[7][2], edit_play_smpl[7][1], edit_play_smpl[7][0]};
+  assign {ss6[5], ss6[1], ss6[4], ss6[2]} = {edit_play_smpl[6][3], edit_play_smpl[6][2], edit_play_smpl[6][1], edit_play_smpl[6][0]};
+  assign {ss5[5], ss5[1], ss5[4], ss5[2]} = {edit_play_smpl[5][3], edit_play_smpl[5][2], edit_play_smpl[5][1], edit_play_smpl[5][0]};
+  assign {ss4[5], ss4[1], ss4[4], ss4[2]} = {edit_play_smpl[4][3], edit_play_smpl[4][2], edit_play_smpl[4][1], edit_play_smpl[4][0]};
+  assign {ss3[5], ss3[1], ss3[4], ss3[2]} = {edit_play_smpl[3][3], edit_play_smpl[3][2], edit_play_smpl[3][1], edit_play_smpl[3][0]};
+  assign {ss2[5], ss2[1], ss2[4], ss2[2]} = {edit_play_smpl[2][3], edit_play_smpl[2][2], edit_play_smpl[2][1], edit_play_smpl[2][0]};
+  assign {ss1[5], ss1[1], ss1[4], ss1[2]} = {edit_play_smpl[1][3], edit_play_smpl[1][2], edit_play_smpl[1][1], edit_play_smpl[1][0]};
+  assign {ss0[5], ss0[1], ss0[4], ss0[2]} = {edit_play_smpl[0][3], edit_play_smpl[0][2], edit_play_smpl[0][1], edit_play_smpl[0][0]};
+
+  sequence_editor seq_edit (.clk(strobe), .rst(reset), .mode(mode), .set_time_idx(seq_sel), .tgl_play_smpl(pb[3:0]), .seq_smpl_1(edit_play_smpl[0]), .seq_smpl_2(edit_play_smpl[1]), 
+                            .seq_smpl_3(edit_play_smpl[2]), .seq_smpl_4(edit_play_smpl[3]), .seq_smpl_5(edit_play_smpl[4]), .seq_smpl_6(edit_play_smpl[5]), .seq_smpl_7(edit_play_smpl[6]), .seq_smpl_8(edit_play_smpl[7]));
+      
+  
+  assign {left[7], left[5], left[3], left[1], 
+          right[7], right[5], right[3], right[1]} = seq_out;
+          
+  logic [3:0] raw_play_smpl;
+  assign raw_play_smpl = pb[3:0];
+  
+  logic [3:0] play_smpl;
+  always_ff @(posedge hz2m, posedge reset) begin
+    if (reset)
+      play_smpl <= 0;
+    else begin
+      case (mode)
+        2'd0: play_smpl <= 4'b0;
+        2'd1: play_smpl <= edit_play_smpl | raw_play_smpl;
+        2'd2: play_smpl <= raw_play_smpl;
+      endcase
+    end
+  end
 endmodule
 
 module scankey (
